054dbbeb177c8c2147c49d7a8b809c80c6c4a2c5
jlbuild <juliabuildbot@gmail.com>
2022-04-01 15:18:18 +0000

    hsakmt_roct_jll build 4.5.2+0

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -2,10 +2,11 @@
 export libhsakmt
 
 using NUMA_jll
+using libdrm_jll
 JLLWrappers.@generate_wrapper_header("hsakmt_roct")
 JLLWrappers.@declare_library_product(libhsakmt, "libhsakmt.so.1")
 function __init__()
-    JLLWrappers.@generate_init_header(NUMA_jll)
+    JLLWrappers.@generate_init_header(NUMA_jll, libdrm_jll)
     JLLWrappers.@init_library_product(
         libhsakmt,
         "lib/libhsakmt.so",
